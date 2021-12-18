ccb7f23209d8025b9e91042b730e929f7d6fdcb8
Shristy Kumari <82372346+Shristy1908@users.noreply.github.com>
2021-12-18 16:54:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("hello world!,This is Shubham");
+var firstName = newFunction();
+console.log(firstName);
+
+console.log("hello world!,This is Shubham");
+console.log("hello world!,This is Shubham");
+console.log("hello world!,This is Shubham"+"5");
+
+function newFunction() {
+    return 'Shubahm';
+}
